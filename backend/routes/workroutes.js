e17be79318ced9c472c94e908bad12f1bb4badc4
davidght1 <davidght1@gmail.com>
2023-05-24 16:24:06 +0300

    modified:   backend/controllers/workoutController.js 	modified:   backend/routes/workroutes.js 	new file:   frontend/.gitignore 	new file:   frontend/README.md 	new file:   frontend/package-lock.json 	new file:   frontend/package.json 	new file:   frontend/public/favicon.ico 	new file:   frontend/public/index.html 	new file:   frontend/public/logo192.png 	new file:   frontend/public/logo512.png 	new file:   frontend/public/manifest.json 	new file:   frontend/public/robots.txt 	new file:   frontend/src/App.js 	new file:   frontend/src/components/Navbar.js 	new file:   frontend/src/index.css 	new file:   frontend/src/index.js 	new file:   frontend/src/pages/Home.js

--- backend/routes/workroutes.js
+++ backend/routes/workroutes.js
@@ -3,6 +3,8 @@ const {
   createWorkout,
   getWorkouts,
   getWorkout,
+  deleteWorkout,
+  updateWorkout,
 } = require("../controllers/workoutController");
 
 const router = express.Router();
@@ -17,13 +19,9 @@ router.get("/:id", getWorkout);
 router.post("/", createWorkout);
 
 //DELETE a new workout
-router.delete("/:id", (req, res) => {
-  res.json({ msg: "DELETE single workout" });
-});
+router.delete("/:id", deleteWorkout);
 
 //UPDATE a new workout
-router.patch("/:id", (req, res) => {
-  res.json({ msg: "UPDATE single workout" });
-});
+router.patch("/:id", updateWorkout);
 
 module.exports = router;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+const Home = () => {
+  const [workouts, setWorkOuts] = useState(null);
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkOuts(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => <p key={workout._id}>{workout.title}</p>)}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
