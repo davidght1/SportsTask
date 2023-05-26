bbf8a03cf77f687e21d40b3558481f12b7b67d9b
David Agam <davidght1@gmail.com>
2023-05-26 19:07:12 +0300

    modified:   backend/controllers/workoutController.js 	modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/public/index.html 	modified:   frontend/src/components/WorkoutDetails.js 	modified:   frontend/src/components/WorkoutsForm.js 	modified:   frontend/src/context/WorkoutContext.js 	modified:   frontend/src/index.css 	modified:   frontend/src/pages/Home.js

--- frontend/src/components/WorkoutsForm.js
+++ frontend/src/components/WorkoutsForm.js
@@ -6,6 +6,7 @@ const WorkoutsForm = () => {
   const [load, setLoad] = useState("");
   const [reps, setReps] = useState("");
   const [error, setError] = useState(null);
+  const [emptyFields, setEmptyFields] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -23,12 +24,14 @@ const WorkoutsForm = () => {
 
     if (!response.ok) {
       setError(json.error);
+      setEmptyFields(json.emptyFields);
     }
     if (response.ok) {
       setTitle("");
       setLoad("");
       setReps("");
       setError(null);
+      setEmptyFields([]);
       console.log("new workout added", json);
       dispatch({ type: "CREATE_WORKOUT", payload: json });
     }
@@ -43,6 +46,7 @@ const WorkoutsForm = () => {
         type="text"
         onChange={(e) => setTitle(e.target.value)}
         value={title}
+        className={emptyFields.includes("title") ? "error" : ""}
       />
 
       <label>Load (in kg)</label>
@@ -50,6 +54,7 @@ const WorkoutsForm = () => {
         type="number"
         onChange={(e) => setLoad(e.target.value)}
         value={load}
+        className={emptyFields.includes("load") ? "error" : ""}
       />
 
       <label>Reps:</label>
@@ -57,6 +62,7 @@ const WorkoutsForm = () => {
         type="number"
         onChange={(e) => setReps(e.target.value)}
         value={reps}
+        className={emptyFields.includes("reps") ? "error" : ""}
       />
 
       <button>Add workout</button>
