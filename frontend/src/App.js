5efe0aec7ebf1070b47b90564c3d2b9ea4ee3750
David Agam <davidght1@gmail.com>
2023-05-28 14:01:41 +0300

    modified:   frontend/src/App.js 	modified:   frontend/src/components/Navbar.js 	new file:   frontend/src/context/AuthContext.js 	new file:   frontend/src/hooks/useAuthContext.js 	modified:   frontend/src/index.js 	new file:   frontend/src/pages/Login.js 	new file:   frontend/src/pages/SignUp.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // pages && components
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar/>
+        <Navbar />
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
           </Routes>
         </div>
       </BrowserRouter>
