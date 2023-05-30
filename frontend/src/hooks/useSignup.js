671fe3ed3831e539198008c146de938db198f8ab
davidght1 <davidght1@gmail.com>
2023-05-30 13:46:56 +0300

    modified:   frontend/src/components/Navbar.js 	new file:   frontend/src/hooks/useLogout.js 	modified:   frontend/src/hooks/useSignup.js 	modified:   frontend/src/index.css

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -11,7 +11,7 @@ export const useSignup = () => {
 
     const response = await fetch("/api/user/signup", {
       method: "POST",
-      headers: { "Content-Type": "application/json", },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
     });
 
